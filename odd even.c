1862f3d5359f54b550160e598fd4615866bd18e1
Turjoy Dhar <dharturjoy5@gmail.com>
2026-09-05 19:01:17 +0530

    Remove unnecessary comments and format code

--- odd even.c
+++ odd even.c
@@ -2,17 +2,12 @@
 
 int main() {
     int num;
-
-    // Ask the user for an integer
     printf("Enter an integer: ");
     scanf("%d", &num);
-
-    // Check if the remainder is 0 when divided by 2
     if (num % 2 == 0) {
         printf("%d is even.\n", num);
     } else {
         printf("%d is odd.\n", num);
     }
-
-    return 0;
+return 0;
 }
