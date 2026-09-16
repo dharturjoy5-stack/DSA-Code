9bb235d1a2779fd8b17249f3f76522f475935e1f
Turjoy Dhar <dharturjoy5@gmail.com>
2026-09-16 21:17:53 +0530

    Add files via upload

---
+++ Stack.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#define MAX 5
+int stack[MAX];
+int top = -1;
+int isFull() {
+    return top == MAX - 1;
+}
+int isEmpty() {
+    return top == -1;
+}
+void push(int value) {
+    if (isFull()) {
+        printf("Stack Overflow\n");
+    } else {
+        top++;
+        stack[top] = value;
+        printf("Push\n");
+    }
+}
+int pop() {
+    if (isEmpty()) {
+        printf("Stack Underflow\n");
+        return -1; // Return an invalid value to indicate underflow
+    } else {
+        int poppedValue = stack[top];
+        top--;
+        printf("Pop\n");
+        return poppedValue;
+    }
+}
+int peak() {
+    if (isEmpty()) {
+        printf("Stack is empty\n");
+        return -1; // Return an invalid value to indicate empty stack
+    } else {
+        return stack[top];
+    }
+}
+void display() {
+    if (isEmpty()) {
+        printf("Stack is empty\n");
+    } else {
+        printf("Stack elements: ");
+        for (int i = top; i >= 0; i--) {
+            printf("%d ", stack[i]);
+        }
+        printf("\n");
+    }
+}
+int main(){
+    push(10);
+    push(20);
+    push(30);
+    display();
+    printf("Top element: %d\n", peak());
+    printf("Popped element: %d\n", pop());
+    display();
+    return 0;
+}
